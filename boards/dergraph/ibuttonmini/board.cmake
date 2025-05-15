2d131f9032f7a4901131af99b17cc389f3ae1550
derGraph <paulantongraf@gmail.com>
2025-05-15 21:54:55 +0200

    fix uploading

--- boards/dergraph/ibuttonmini/board.cmake
+++ boards/dergraph/ibuttonmini/board.cmake
@@ -1,9 +1,24 @@
 # SPDX-License-Identifier: Apache-2.0
 
-#board_runner_args(jlink "--device=nRF51822_xxAA" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
+set(OPENOCD_NRF5_INTERFACE "stlink")
+set(BOARD_DEBUG_RUNNER openocd)
 
-set(BOARD_FLASH_RUNNER openocd)
+if(NOT DEFINED OPENOCD_NRF5_SUBFAMILY)
+  string(REGEX MATCH nrf5. OPENOCD_NRF5_SUBFAMILY "${BOARD}")
+
+  if(HWMv2 AND "${OPENOCD_NRF5_SUBFAMILY}" STREQUAL "")
+    string(REGEX MATCH nrf5. OPENOCD_NRF5_SUBFAMILY "${BOARD_QUALIFIERS}")
+  endif()
+endif()
+
+set(pre_init_cmds
+  "set WORKAREASIZE 0x4000"	# 16 kB RAM used for flashing
+  "source [find interface/${OPENOCD_NRF5_INTERFACE}.cfg]"
+  "source [find target/${OPENOCD_NRF5_SUBFAMILY}.cfg]"
+)
+
+foreach(cmd ${pre_init_cmds})
+  board_runner_args(openocd --cmd-pre-init "${cmd}")
+endforeach()
+
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
