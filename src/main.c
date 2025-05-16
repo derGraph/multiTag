de3cc2eed62d2a94d22b274ecb607eccbb016b7e
derGraph <paulantongraf@gmail.com>
2025-05-16 16:57:05 +0200

    add module say_hello, next rename + add code

--- src/main.c
+++ src/main.c
@@ -7,25 +7,31 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include "fhn_eid_generator.h"
 
 /* 1000 msec = 1 sec */
 #define SLEEP_TIME_MS   1000
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED0_NODE DT_ALIAS(led0)
+#define BUTTON0_NODE DT_ALIAS(sw0)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+static const struct gpio_dt_spec button = GPIO_DT_SPEC_GET(BUTTON0_NODE, gpios);
 
 int main(void)
 {
 	int ret;
-	bool led_state = true;
 
 	if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+	if(!gpio_is_ready_dt(&button)) {
 		return 0;
 	}
 
@@ -34,15 +40,19 @@ int main(void)
 		return 0;
 	}
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
-		if (ret < 0) {
-			return 0;
-		}
+	ret = gpio_pin_configure_dt(&button, GPIO_INPUT);
+	if (ret < 0) {
+		return 0;
+	}
 
-		led_state = !led_state;
-		printf("LED state: %s\n", led_state ? "ON" : "OFF");
-		k_msleep(SLEEP_TIME_MS);
+	while (1) {
+		if(gpio_pin_get_dt(&button)){
+			ret = gpio_pin_toggle_dt(&led);
+			if (ret < 0) {
+				return 0;
+			}
+			while(gpio_pin_get_dt(&button));
+		}
 	}
 	return 0;
 }
